2c9593afcc5998b59f2834939955905d6888677f
SYED NABIL <89401427+SNFASA@users.noreply.github.com>
2023-02-18 15:34:45 +0800

    Add files via upload

---
+++ CGPA.java
@@ -0,0 +1,17 @@
+import java.util.Scanner ;
+public class CGPA{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your CGPA : ");
+        double cgpa = sc.nextDouble();
+        if (cgpa >= 3.75)
+            System.out.println("You are an excellent student");
+       
+        
+        
+        
+       
+        
+    
+    }
+}
